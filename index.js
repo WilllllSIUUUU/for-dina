39d2e528bba87537179a52186bf848a86a395de9
WilllllSIUUUU <105867541+WilllllSIUUUU@users.noreply.github.com>
2025-05-28 19:47:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `I Have Something`.split('')
+const text = `aku punya sesuatu buat kamu dina`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
